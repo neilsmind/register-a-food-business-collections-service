74542c19d9cb94006dde4cbfd50d147c66014047
Django Shelton <djangoshelton@gmail.com>
2018-10-31 15:18:49 +0000

    add logging mock

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   verbose: true,
   testEnvironment: "node",
+  moduleNameMapper: {
+    "logging.service": "<rootDir>/src/__mocks__/logging.service.js"  
+  },
   collectCoverageFrom: [
     "**/*.{js}",
     "!**/node_modules/**",
@@ -8,6 +11,7 @@ module.exports = {
     "!**/tests/*",
     "!**/coverage/**",
     "!**/src/app.js",
-    "!**/src/api/routers.js"
+    "!**/src/api/routers.js",
+    "!**/src/db/**"
   ]
 };
