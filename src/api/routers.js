d3adae3d4ee5a77a854aec68e54e477414363722
Django Shelton <djangoshelton@gmail.com>
2018-11-12 09:05:38 +0000

    link up db

--- src/api/routers.js
+++ src/api/routers.js
@@ -1,10 +1,13 @@
 const { Router } = require("express");
-
+const {
+  getAllRegistrationsByCouncil
+} = require("../connectors/registrationDb/registrationDb");
 const routers = () => {
   const router = Router();
 
-  router.get("/api/collect", (req, res) => {
-    res.send("hello world");
+  router.get("/api/collect/:lc", async (req, res) => {
+    const allLocalCouncils = await getAllRegistrationsByCouncil(req.params.lc);
+    res.send(allLocalCouncils);
   });
 
   return router;
