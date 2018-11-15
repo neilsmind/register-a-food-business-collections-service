08bda6f908c30a650d7fa8544af4e7d39d7e60ef
A. P. Malde <anisha.malde@ibm.com>
2018-11-15 15:06:52 +0000

    component tests draft

--- tests/component/helloWorld.component.test.js
+++ tests/component/helloWorld.component.test.js
@@ -2,10 +2,44 @@ const request = require("request-promise-native");
 const url = "http://localhost:4001/api/collect";
 
 describe("Collections Route: ", () => {
-  describe("On a GET request to /api/collect", () => {
-    it("Should return 'hello world'", async () => {
+  describe("On a GET request to /api/collect/:lc/all", () => {
+    beforeEach(async () => {
       const response = await request(url);
-      expect(response).toBe("hello world");
     });
+    it("Should return an array with x number of entries", () => {
+      expect(response.length).toBe("x");
+    });
+    it("Should return an array, whose first object contains operator, registrations, premise, activities and metadata", () => {
+      expect(result[0].registration.fsa_rn).toBe("BLAH");
+      expect(result[0].establishment.establishment_trading_name).toBe("BLAH");
+      expect(result[0].operator.operator_name).toBe("BLAH");
+      expect(result[0].premise.establishment_postcode).toBe("BLAH BLAH");
+      expect(result[0].metadata.declaration1).toBe("BLAH");
+      expect(result[0].registration.collected).toBe(true)
+    });
+  });
+  describe("On a GET request to /api/collect/:lc/all", () => {
+    beforeEach(async () => {
+      const response = await request(url);
+    });
+    it("Should return an array with x number of entries'", () => {
+      expect(response.length).toBe("x");
+    });
+    it("Should return an array, whose first object contains operator, registrations, premise, activities and metadata", () => {
+      expect(result[0].registration.fsa_rn).toBe("BLAH");
+      expect(result[0].establishment.establishment_trading_name).toBe("BLAH");
+      expect(result[0].operator.operator_name).toBe("BLAH");
+      expect(result[0].premise.establishment_postcode).toBe("BLAH BLAH");
+      expect(result[0].metadata.declaration1).toBe("BLAH");
+    });
+    it("Should return an array without the collected and collected_at fields", () => {
+      expect(result[0].registration.collected).toBeUndefined();
+      expect(result[0].registration.collected_at).toBeUndefined();
+    });
+    describe("When called twice", () => {
+      beforeEach(async () => {
+        const response1 = await request(url);  
+        const response2 = await request(url);        
+      });
   });
 });
