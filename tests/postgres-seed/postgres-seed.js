cc5e0f530601b6d32f2ad436300a9cc123bdeaae
Simon Thomas <simon.thomas@civica.co.uk>
2020-03-30 11:41:10 +0000

    update seeding to force deferred saving to DB

--- tests/postgres-seed/postgres-seed.js
+++ tests/postgres-seed/postgres-seed.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const request = require("request-promise-native");
 const mockRegistrationData = require("./mock-registration-data.json");
 
-const seedPostgres = async () => {
+const submitRegistration = async () => {
   const url = process.env.SERVICE_API_URL;
 
   const responses = [];
@@ -24,10 +24,34 @@ const seedPostgres = async () => {
     const response = await request(requestOptions);
     responses.push(response);
   }
+}
+
+const forceRegistrationSubmission = async (submissionResult) => {
+  const url = process.env.COMPONENT_TEST_BASE_URL + "/savetotempstore/";
+
+  const responses = [];
+
+  for (let registration in submissionResult) {
+    const requestOptions = {
+      uri: url + "?fsaID=" + registration.fsa-rn,
+      method: "GET",
+      json: true,
+      body: null,
+      headers: {
+        "api-secret": process.env.SERVICE_API_SECRET,
+        "client-name": process.env.SERVICE_API_CLIENT_NAME,
+        "registration-data-version": process.env.REGISTRATION_DATA_VERSION
+      }
+    };
+
+    const response = await request(requestOptions);
+    responses.push(response);
+  }
 
   return responses;
 };
 
-seedPostgres().then(result => {
-  console.log(result);
-});
+submitRegistration()
+  .then(submissionResult => forceRegistrationSubmission(submissionResult))
+  .then(seedResult => console.log(seedResult));
+
