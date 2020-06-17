e991fc627c4be06757c1c1482abcd6807682c26d
Adam White <awhite234@gmail.com>
2020-06-17 10:13:57 +0100

    Sync with registrationservice.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
+const cls = require("cls-hooked");
 const appInsights = require("applicationinsights");
+const morgan = require("morgan");
 
 if (
   "APPINSIGHTS_INSTRUMENTATIONKEY" in process.env &&
@@ -6,6 +8,20 @@ if (
 ) {
   appInsights.setup().start();
 }
+
+const clsNamespace = cls.createNamespace("application");
+
+const clsMiddleware = (req, res, next) => {
+  // req and res are event emitters. We want to access CLS context inside of their event callbacks
+  clsNamespace.bind(req);
+  clsNamespace.bind(res);
+
+  clsNamespace.run(() => {
+    clsNamespace.set("request", req);
+
+    next();
+  });
+};
 
 const { logger } = require("./services/winston");
 const express = require("express");
@@ -18,10 +34,12 @@ const { errorHandler } = require("./middleware/errorHandler");
 const app = express();
 const port = process.env.PORT || 4001;
 
+app.use(clsMiddleware);
 app.use(helmet());
 app.use(bodyParser.json());
 app.use("/", routers());
 app.use(errorHandler);
+app.use(morgan("combined", { stream: logger.stream }));
 
 app.listen(port, () => {
   logger.info(`Collections service started listening on port ${port}`);
