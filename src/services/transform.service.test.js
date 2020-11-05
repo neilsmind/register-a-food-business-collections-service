402574b89cd4be3bc234185075519df17f54ded3
Joshua Perry <joshua.perry@civica.co.uk>
2020-11-05 13:33:38 +0000

    Add unit tests for transform service

---
+++ src/services/transform.service.test.js
@@ -0,0 +1,153 @@
+const { transformEnums } = require("./transform.service");
+
+let registrations = [
+  (registration1 = {
+    establishment: {
+      establishment_details: {
+        establishment_trading_name: "Testing name",
+        establishment_primary_number: "09876 54321"
+      },
+      operator: {
+        operator_first_name: "Tom",
+        operator_last_name: "Healey",
+        operator_type: "Sole trader"
+      },
+      premise: {
+        establishment_postcode: "NR14 7PZ",
+        establishment_town: "Norwich",
+        establishment_type: "Mobile or moveable premises",
+        establishment_address_line_1: "Test 1 Ltd"
+      },
+      activities: {
+        customer_type: "End consumer",
+        business_type: "Any other retailer",
+        import_export_activities: "None",
+        water_supply: "Private"
+      }
+    }
+  }),
+  (registration2 = {
+    establishment: {
+      establishment_details: {
+        establishment_trading_name: "Trading name",
+        establishment_primary_number: "01234 456789"
+      },
+      operator: {
+        operator_first_name: "Jeff",
+        operator_last_name: "Healey",
+        operator_type: "SOLETRADER"
+      },
+      premise: {
+        establishment_postcode: "NR14 7PZ",
+        establishment_town: "Norwich",
+        establishment_type: "MOBILE",
+        establishment_address_line_1: "Test 2 Ltd"
+      },
+      activities: {
+        customer_type: "ENMD_CONSUMER",
+        business_type: "Any other retailer",
+        import_export_activities: "NONE",
+        water_supply: "PRIVATE"
+      }
+    }
+  })
+];
+
+describe("transform.service", () => {
+  describe("When api version is >= 2", () => {
+    describe("When passed a single registration", () => {
+      beforeEach(() => {
+        transformEnums("2", registrations[0]);
+      });
+
+      it("Should only tranform enum values to enum keys", () => {
+        expect(registrations[0].establishment.operator.operator_type).toEqual(
+          "SOLETRADER"
+        );
+        expect(
+          registrations[0].establishment.operator.operator_first_name
+        ).toEqual("Tom");
+      });
+    });
+    describe("When passed an array of registration", () => {
+      beforeEach(() => {
+        transformEnums("2", registrations);
+      });
+
+      it("Should only tranform enum values to enum keys", () => {
+        expect(
+          registrations[0].establishment.premise.establishment_type
+        ).toEqual("MOBILE");
+        expect(
+          registrations[1].establishment.premise.establishment_type
+        ).toEqual("MOBILE");
+        expect(
+          registrations[1].establishment.operator.operator_first_name
+        ).toEqual("Jeff");
+      });
+    });
+  });
+  describe("When api version is latest", () => {
+    describe("When passed a single registration", () => {
+      beforeEach(() => {
+        transformEnums("latest", registrations[0]);
+      });
+      it("Should only tranform enum values to enum keys", () => {
+        expect(registrations[0].establishment.activities.customer_type).toEqual(
+          "END_CONSUMER"
+        );
+        expect(
+          registrations[0].establishment.operator.operator_first_name
+        ).toEqual("Tom");
+      });
+    });
+    describe("When passed an array of registration", () => {
+      beforeEach(() => {
+        transformEnums("latest", registrations);
+      });
+      it("Should only tranform enum values to enum keys", () => {
+        expect(
+          registrations[0].establishment.activities.import_export_activities
+        ).toEqual("NONE");
+        expect(
+          registrations[1].establishment.activities.import_export_activities
+        ).toEqual("NONE");
+        expect(
+          registrations[1].establishment.operator.operator_first_name
+        ).toEqual("Jeff");
+      });
+    });
+  });
+  describe("When api version is < 2", () => {
+    describe("When passed a single registration", () => {
+      beforeEach(() => {
+        transformEnums("1", registrations[1]);
+      });
+      it("Should only tranform enum keys to enum values", () => {
+        expect(registrations[1].establishment.activities.water_supply).toEqual(
+          "Private"
+        );
+        expect(
+          registrations[0].establishment.operator.operator_first_name
+        ).toEqual("Tom");
+      });
+    });
+    describe("When passed an array of registrations", () => {
+      beforeEach(() => {
+        transformEnums("1", registrations);
+      });
+
+      it("Should only tranform enum keys to enum values", () => {
+        expect(
+          registrations[1].establishment.activities.import_export_activities
+        ).toEqual("None");
+        expect(
+          registrations[0].establishment.activities.import_export_activities
+        ).toEqual("None");
+        expect(
+          registrations[1].establishment.operator.operator_first_name
+        ).toEqual("Jeff");
+      });
+    });
+  });
+});
